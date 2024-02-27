66143e58b2e9ac65cc2846d03ba3fe6a1c4641a8
Bilal <86686062+Bilal-A-G@users.noreply.github.com>
2024-02-27 09:34:38 -0500

    Initial commit, included glfw and glm in project

---
+++ GenerateProjectFilesWin.bat
@@ -0,0 +1,2 @@
+call external\premake-5.0.0_win\premake5.exe vs2022
+PAUSE
---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "Project Risk"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Engine"
+    location "Engine"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    libdirs
+    {
+        "%{prj.name}/lib",
+    }
+
+    includedirs
+    {
+        "%{prj.name}/include",
+    }
+
+    defines
+    {
+        "_CRT_SECURE_NO_WARNINGS",
+        "ENGINE"
+    }
+
+    links
+    {
+        "glfw3_mt"
+    }
+
+    files
+    {
+        "%{prj.name}/**.h",
+        "%{prj.name}/**.cpp",
+        "%{prj.name}/**.c",
+        "%{prj.name}/**.glsl"
+    }
+
+    filter "system:Windows"
+        defines
+        {"WIN",}
+
+    filter "configurations:Debug"
+        defines "DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "RELEASE"
+        optimize "speed"
+
+    filter "files:**.c"
+        flags {"NoPCH"}
+
+
+project "Client"
+    location "Client"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    staticruntime "On"
+
+    includedirs
+    {
+        "Engine/src",
+        "Engine/include"    
+    }
+
+    files
+    {
+        "%{prj.name}/**.h",
+        "%{prj.name}/**.cpp",
+        "%{prj.name}/**.c",
+        "%{prj.name}/**.glsl"
+    }
+
+    links
+    {
+        "Engine",
+    }
+
+    filter "system:Windows"
+        defines
+        {"WIN",}
+
+    filter "configurations:Debug"
+        defines "DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "RELEASE"
+        optimize "speed"
